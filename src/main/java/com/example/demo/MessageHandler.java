e1ce85ad27390902f6f087fa4f79f0474fec8707
Eduardo Marval <eduardom@rightmove.co.uk>
2023-03-06 18:41:46 +0000

    - Added the logic to capture any IllegalArgumentException and retry 3 times with a delay of 2 second and a back off multiplier of 3 before sending the original message to the specific dead-letter-queue - Added the logic to the converter to throw a IllegalArgumentException if the name of the user is 'Testy' - Added the console output in the converter to show the retries and the time when the retry happens

--- src/main/java/com/example/demo/MessageHandler.java
+++ src/main/java/com/example/demo/MessageHandler.java
@@ -12,11 +12,18 @@ public class MessageHandler extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        onException(IllegalArgumentException.class)
+                .useOriginalMessage()
+                .handled(true)
+                .maximumRedeliveries("3")
+                .redeliveryDelay("2000")
+                .backOffMultiplier("3")
+                .to("activemq:queue:user-dead-letter-queue");
+
         onException(Exception.class)
                 .useOriginalMessage()
                 .handled(true)
                 .to("activemq:queue:user-dead-letter-queue");
-
 
         from("activemq:queue:test-consuming")
                 .log("==============This is the Message==============")
--- src/main/java/com/example/demo/converter/UserDetailsConverter.java
+++ src/main/java/com/example/demo/converter/UserDetailsConverter.java
@@ -4,10 +4,20 @@ import com.example.demo.entities.CompactUserDetails;
 import com.example.demo.entities.UserDetails;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
 @Component
 public class UserDetailsConverter {
 
     public CompactUserDetails userDetailsToCompactUserDetails(UserDetails userDetails){
+        System.out.println("==========Hello from the converter: "
+                + LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS) +
+                "====================");
+
+        if(userDetails.getName().equals("Testy")){
+            throw new IllegalArgumentException("Testy is not a supported user name");
+        }
         return new CompactUserDetails(userDetails.getName() + " " + userDetails.getSurname());
     }
 }
